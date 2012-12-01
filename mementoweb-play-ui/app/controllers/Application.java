2f560a4981746833ddb3d98aae0fc1bf02965b13
Andrew Jackson <Andrew.Jackson@bl.uk>
2012-12-01 22:10:57 +0000

    Updated to allow form to work correctly, submitting and re-directing as appropriate.

--- mementoweb-play-ui/app/controllers/Application.java
+++ mementoweb-play-ui/app/controllers/Application.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import play.*;
+import play.cache.Cache;
 import play.data.*;
 import play.mvc.*;
 import play.libs.Json;
@@ -8,6 +9,7 @@ import play.libs.Json;
 import views.html.*;
 
 import models.Task;
+import models.Query;
 
 import uk.bl.wap.memento.MementoSearchBean;
 import uk.bl.wap.memento.jsf.MementoBackingBean;
@@ -59,11 +61,39 @@ public class Application extends Controller {
        }
   }
 
-  public static Result findMementos(String url) {
-    MementoBackingBean msb = new MementoBackingBean();
-    msb.setUrl(url);
+  static Form<Query> queryForm = form(Query.class);
+
+  public static Result queryMementos() {
     return ok(
-      views.html.search.render(msb)
+      views.html.search.render(new MementoBackingBean(), queryForm)
+    );
+  }
+
+  public static Result findMementos() {
+    Form<Query> urlParam = queryForm.bindFromRequest();
+    System.err.println("ERROR "+urlParam.hasErrors());
+    if(urlParam.hasErrors()) {
+        return badRequest(
+          views.html.search.render(new MementoBackingBean(), urlParam)
+        );
+    }
+    Query q = urlParam.get();
+    return redirect(routes.Application.findMementosFor(q.url));
+//    return findMementosFor(q.url);
+  }
+
+  public static Result findMementosFor(String url) {
+    MementoBackingBean msb = (MementoBackingBean) Cache.get("Mementos."+url);
+    if( msb == null ) {
+      msb = new MementoBackingBean();
+      msb.setUrl(url);
+    }
+    Cache.set("Mementos."+url, msb);
+    // Rebuild a Query object.
+    Query q = new Query();
+    q.url = url;
+    return ok(
+      views.html.search.render(msb, queryForm.fill(q))
     );
   }
 
