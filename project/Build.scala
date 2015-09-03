2298d005c71f7c44f140fd59565c6cca97a994eb
Andrew Jackson <Andrew.Jackson@bl.uk>
2015-09-03 15:23:14 +0100

    Move to release.

--- project/Build.scala
+++ project/Build.scala
@@ -11,7 +11,7 @@ object ApplicationBuild extends Build {
       javaCore,
       cache,
       // Add your project dependencies here,
-      "uk.bl.wa.memento" % "mementoweb-client-java" % "1.1.7-SNAPSHOT"
+      "uk.bl.wa.memento" % "mementoweb-client-java" % "1.1.7"
     )
 
 val main = play.Project(appName, appVersion, appDependencies).settings(
